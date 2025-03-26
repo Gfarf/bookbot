18b81a36af022771d40262187f95395987521b80
Gfarf <gfpalu@gmail.com>
2025-03-25 21:50:32 -0300

    primeiras funções executadas

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def get_book_text (path_to_file):
+    with open(path_to_file) as f:
+        contents = f.read()
+    return contents
+
+def main ():
+    pathf = "./books/frankenstein.txt"
+    texto = get_book_text(pathf)
+    count = 0
+    for i in texto.split():
+        count += 1
+    print(f"{count} words found in the document")
+    return
+
+main()
